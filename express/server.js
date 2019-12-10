e14f23f9993c628155a43f198d3a9c19fef9c969
Dy2io <dyziolonger@gmail.com>
2019-12-10 19:08:45 +0100

    Update server

--- express/server.js
+++ express/server.js
@@ -1,22 +1,92 @@
-'use strict';
 const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+const con = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "engineerwork2"
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+async function select(x, y) {
+    const longitude = []; //szerokość e,w
+    const latitude = []; //długość n,s
+    let i;
+    let j = 0.0;
+    for (i = 0; i < 61; ++i) {
+        latitude[i] = j;
+        j = j + 1.5;
+    }
 
-module.exports = app;
-module.exports.handler = serverless(app);
+    let k;
+    let l = 0.0;
+    for (k = 0; k < 121; ++k) {
+        longitude[k] = l;
+        l = l + 1.5;
+    }
+
+    const lonmin = [];
+    const lonmax = [];
+    const latmin = [];
+    const latmax = [];
+
+    for (i = 0; i < 121; ++i) {
+        if (longitude[i] < x) {
+            lonmin.push(longitude[i])
+        } else {
+            lonmax.push(longitude[i])
+        }
+    }
+
+    for (i = 0; i < 61; ++i) {
+        if (latitude[i] < y) {
+            latmin.push(latitude[i])
+        } else {
+            latmax.push(latitude[i])
+        }
+    }
+
+    const z11 = "SELECT `" + lonmin[lonmin.length - 1] + "` FROM ne WHERE stopnie=" + latmin[latmin.length - 1] + "";
+    const z12 = "SELECT `" + lonmin[lonmin.length - 1] + "` FROM ne WHERE stopnie=" + latmax[0] + ""
+    const z21 = "SELECT `" + lonmax[0] + "` FROM ne WHERE stopnie=" + latmin[latmin.length - 1] + "";
+    const z22 = "SELECT `" + lonmax[0] + "` FROM ne WHERE stopnie=" + latmax[0] + "";
+
+    let q11, q12, q21, q22;
+    let q11t, q12t, q21t, q22t;
+    let q11r, q12r, q21r, q22r;
+
+    q11 = await con.query(z11);
+    q12 = await con.query(z12);
+    q21 = await con.query(z21);
+    q22 = await con.query(z22);
+
+    q11t = q11[0][0];
+    q12t = q12[0][0];
+    q21t = q21[0][0];
+    q22t = q22[0][0];
+
+    q11r = q11t['' + lonmin[lonmin.length - 1]];
+    q12r = q12t['' + lonmin[lonmin.length - 1]];
+    q21r = q21t['' + lonmax[0]];
+    q22r = q22t['' + lonmax[0]];
+
+    return [q11r, q12r, q21r, q22r];
+}
+
+app.use(cors());
+app.get('/q0', async function(req, res) {
+    const x0 = req.query.x1;
+    const y0 = req.query.y1;
+    const data = await select(x0, y0);
+    res.send(data);
+    console.log(data)
+})
+
+
+app.listen(5698, function() {
+    console.log('Link do serwera:' + 'http://localhost:5698/q0?x1=18&y1=18');
+
+})
